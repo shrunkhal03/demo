05263c396f64448ce573885e57fd4b85e7ec0d79
shrunkhal03 <shrunkhal4@gmail.com>
2025-07-26 10:12:29 +0530

    updated

---
+++ src/Demo.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        List<Employee> list = Arrays.asList(new Employee(1L, "sham", 6000),
+                new Employee(2L,"ram",300),
+                new Employee(3L,"bam",200),
+                new Employee(3L,"Shanku",5000));
+
+        List<String> collect = list.stream().filter(n -> n.getSalary() >= 20000).map(Employee::getName).collect(Collectors.toList());
+        System.out.println(collect);
+
+        List<Employee> sorted = list.stream().sorted(Comparator.comparing(Employee::getSalary)).toList();
+        System.out.println(sorted);
+
+        list.stream().sorted(Comparator.comparing(Employee::getSalary).reversed()).skip(1).limit(1).forEach(System.out::println);
+    }
+
+
+}
---
+++ src/Employee.java
@@ -0,0 +1,44 @@
+public class Employee {
+    public Employee(Long id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    private Long id;
+    private String name;
+    private int salary;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
